8322fcc72a82ebf79bf0c7a05d21b58d64dfadf7
nblancoz <nblancozappala@gmail.com>
2023-12-14 11:00:46 +0100

    first commit

---
+++ src/app/components/characters/characters.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CharacterInterface } from '../../interfaces/character-interface';
+import { CharacterService } from '../../services/character.service';
+
+@Component({
+  selector: 'app-characters',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.scss',
+})
+export class CharactersComponent {
+  public characters: Array<CharacterInterface> = [];
+
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.characterService.getCharacters().subscribe({
+      next: (data) => {
+        this.characters = data.results;
+      },
+      error: (error) => {
+        console.error('Error fetching characters:', error);
+      },
+    });
+  }
+}
